91353405c0eace38877dc88132d9d49a14844a30
Wael <wael.hawa86@gmail.com>
2021-04-07 19:00:39 -0400

    first commit

---
+++ Exerceise1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Exerceise1
+    {
+    class Program
+        {
+        static void Main(string[] args)
+            {
+            //Part 1
+
+            string name;
+            string text;
+            int age;
+            int years;
+            Console.Write("Input your name: ");
+            name = Console.ReadLine();
+            Console.Write("Input age: ");
+            text = Console.ReadLine();
+            age = int.Parse(text);
+            Console.Write("How many years have passed: ");
+            text = Console.ReadLine();
+            years = int.Parse(text);
+            Console.WriteLine($"Your name is {name}. You will be {age + years} years old in {years} years");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            //Part2
+
+            int length;
+            int width;
+            Console.Write("Input room Length: ");
+            text = Console.ReadLine();
+            length = int.Parse(text);
+            Console.Write("Input room Width: ");
+            text = Console.ReadLine();
+            width = int.Parse(text);
+            Console.WriteLine($"The area of the room is {length * width}");
+            }
+        }
+    }
